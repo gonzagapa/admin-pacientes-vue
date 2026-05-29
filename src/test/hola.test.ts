771bced5c9953a0f621a14f443f3e4f904db4ccc
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2026-05-28 21:55:59 -0600

    chore: init test suit

---
+++ src/test/hola.test.ts
@@ -0,0 +1,5 @@
+describe('Suma dos numeros', () => {
+    it('Debe sumar 2 numeros correctamente', () => {
+        expect(2 + 2).toBe(4);
+    })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+  },
   plugins: [vue(), tailwindcss()],
 })
